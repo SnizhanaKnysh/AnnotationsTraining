0a5a0f0f94fa878c14ce9cd45e1f652fa0704828
Snizhana_K <snizhana.knysh@intellecteu.com>
2024-11-20 14:15:22 +0200

    Add HW for reflection & annotations

--- src/main/java/app/ArrayUtils.java
+++ src/main/java/app/ArrayUtils.java
@@ -1,4 +1,39 @@
 package app;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface MethodInfo {
+    String methodName();
+    String returnType();
+    String description();
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface AuthorInfo {
+    String firstName();
+    String lastName();
+}
+
 public class ArrayUtils {
+
+    String book1 = "Book 1";
+    String book2 = "Book 2";
+
+    @MethodInfo(methodName = "processData", returnType = "void", description = "this is a method to check annotations functionality")
+    public void processData() {
+        System.out.println(book1);
+        System.out.println(book2);
+    }
+
+    @AuthorInfo(firstName = "Snizhana", lastName = "Knysh")
+    public void checkAnnotationForAuthor() {
+        System.out.println("Printing some dummy values :)");
+    }
+
 }
--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,4 +1,44 @@
 package app;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+
 public class Main {
+
+    public static void main(String[] args)
+        throws NoSuchMethodException, ClassNotFoundException, InvocationTargetException, IllegalAccessException {
+
+        System.out.println("\n   Checking MethodInfo annotation");
+        MethodInfo methodInfo = ArrayUtils.class.getDeclaredMethod("processData").getAnnotation(MethodInfo.class);
+
+        System.out.println("methodInfo: " + methodInfo.methodName());
+        System.out.println("methodInfo: " + methodInfo.returnType());
+        System.out.println("methodInfo: " + methodInfo.description());
+
+        System.out.println("\n   Get annotations via Reflection");
+
+        Class<?> clazz = Class.forName("app.ArrayUtils");
+        Method[] methods = clazz.getDeclaredMethods();
+
+        for (Method method : methods) {
+            System.out.println("\n  For Method: " + method.getName());
+            Annotation[] annotations = method.getAnnotations();
+
+            for (Annotation annotation : annotations) {
+                System.out.println("Annotation: " + annotation.annotationType().getName());
+                Method[] annotationMethods = annotation.annotationType().getDeclaredMethods();
+
+                for (Method annotationMethod : annotationMethods) {
+                    Object value = annotationMethod.invoke(annotation);
+                    System.out.println("Field: " + annotationMethod.getName() + ", Value: " + value);
+
+                }
+            }
+
+        }
+
+    }
+
 }
